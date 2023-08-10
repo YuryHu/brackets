8e452e28e5872d7dba8e7eb079dd707c21adc3e3
YuryHu <yhu06351@gmail.com>
2023-08-10 23:58:12 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig =  bracketsConfig.map(e => e.join(''));
+  let contains = true;
+  while (contains) {
+    bracketsConfig.forEach(e => {
+      str = str.replaceAll(e, '');
+    });
+    contains = false;
+    bracketsConfig.forEach(e => {
+      if (str.includes(e)) {
+        contains  =  true;
+      }
+    });
+  }
+  return !str.length;
 }
